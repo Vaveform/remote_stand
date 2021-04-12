675d54f27327e68daa1208c1d5a5114aaf761339
Максим Поставцев <mvpostavtsev@edu.hse.ru>
2021-04-12 08:53:08 +0300

    Added StringExtensions methods
    Added StringExtenstions methods for working with received JSON from browser

---
+++ WebRTC_Remote_FPGA_stand/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebRTC_Remote_FPGA_stand
+{
+    public static class StringExtensions
+    {
+        public static (string, string) DivideHeaderAndOriginalJSON(this string JsonStr)
+        {
+            int index = JsonStr.IndexOf(':', JsonStr.IndexOf(':') + 1);
+            if (index == -1)
+            {
+                return ("", JsonStr);
+            }
+            return (JsonStr.Substring(0, index + 1), JsonStr.Substring(index + 1, JsonStr.Length - index - 3));
+        }
+    }
+}
