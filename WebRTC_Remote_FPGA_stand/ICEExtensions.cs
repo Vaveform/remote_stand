e372fc8f61a572f46dcd195809522303deb0151d
Максим Поставцев <mvpostavtsev@edu.hse.ru>
2021-04-12 09:43:45 +0300

    Added Json Extensions to IceCandidate class
    Added extensions for IceCandidate for serialization to JSON.

---
+++ WebRTC_Remote_FPGA_stand/ICEExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.MixedReality.WebRTC;
+using System.Text.Json;
+
+namespace WebRTC_Remote_FPGA_stand
+{
+    public static class ICEExtensions
+    {
+        /// <summary>
+        ///  Standard JSON serialization for ICE
+        ///  candidate 
+        /// </summary>
+        public static string ToJson(this IceCandidate candidate)
+        {
+            return JsonSerializer.Serialize(new ICEJavaScriptNotation(candidate));
+        }
+
+        /// <summary>
+        ///  JSON format serialization for
+        ///  communication with Artem Baskal 
+        ///  frontend
+        /// </summary>
+        public static string ToABJson(this IceCandidate candidate)
+        {
+            return "{\"data\":{\"candidate\":" + ToJson(candidate) + "}}";
+        }
+    }
+}
