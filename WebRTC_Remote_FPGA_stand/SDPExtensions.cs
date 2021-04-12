909d806a4f7e326383150878daa5f2cc90588c76
Максим Поставцев <mvpostavtsev@edu.hse.ru>
2021-04-12 08:57:25 +0300

    Added extension methods for SDP processing
    Added extension method for sending SDP in JSON format to the browser

---
+++ WebRTC_Remote_FPGA_stand/SDPExtensions.cs
@@ -0,0 +1,36 @@
+﻿/* 
+ * This extensions methods provide serialization format
+ * for communication with client browser, which 
+ * send/receive SDP offers and answers for establish 
+ * WebRTC connection
+*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.MixedReality.WebRTC;
+using System.Web;
+
+namespace WebRTC_Remote_FPGA_stand
+{
+    public static class SDPExtensions
+    {
+        /// <summary>
+        ///  Standard JSON serialization for session
+        ///  description protocol 
+        /// </summary>
+        public static string ToJson(this SdpMessage SDP) {
+            return "{\"type\":\"" + SdpMessage.TypeToString(SDP.Type) + 
+                "\",\"sdp\":\"" + HttpUtility.JavaScriptStringEncode(SDP.Content) + 
+                "\"}";
+        }
+
+        /// <summary>
+        ///  JSON format serialization for
+        ///  communication with Artem Baskal 
+        ///  frontend
+        /// </summary>
+        public static string ToABJson(this SdpMessage SDP) {
+            return "{\"data\":{\"description\":" + ToJson(SDP) + "}}";
+        }
+    }
+}
