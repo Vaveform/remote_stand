64e47d4c5ea49cccabae9fb784cc61c33c685637
Максим Поставцев <mvpostavtsev@edu.hse.ru>
2021-04-12 09:00:01 +0300

    Added types for connectivity with JS notation
    Added types for connectivity between MixedRealityWebRTC and original JS WebRTC API

---
+++ WebRTC_Remote_FPGA_stand/JavaScriptNotation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Microsoft.MixedReality.WebRTC;
+
+namespace WebRTC_Remote_FPGA_stand
+{
+    public interface IConvertToMRWebRTCNotation<T>
+    {
+        public T ToMRNetCoreNotation();
+    }
+    public class ICEJavaScriptNotation : IConvertToMRWebRTCNotation<IceCandidate>
+    {
+
+        public ICEJavaScriptNotation(IceCandidate ice) {
+            candidate = ice.Content;
+            sdpMid = ice.SdpMid;
+            sdpMLineIndex = ice.SdpMlineIndex;
+        }
+        public ICEJavaScriptNotation() { }
+        public IceCandidate ToMRNetCoreNotation () {
+            return new IceCandidate { Content = candidate, SdpMid = sdpMid, SdpMlineIndex = sdpMLineIndex};
+        }
+        public string candidate { get; set; }
+        public string sdpMid { get; set; }
+        public int sdpMLineIndex { get; set; }
+    }
+    public class SDPJavaScriptNotation : IConvertToMRWebRTCNotation<SdpMessage>
+    {
+        public SDPJavaScriptNotation(SdpMessage session_description)
+        {
+            type = SdpMessage.TypeToString(session_description.Type);
+            sdp = session_description.Content;
+        }
+        public SDPJavaScriptNotation() { }
+        public SdpMessage ToMRNetCoreNotation()
+        {
+            return new SdpMessage { Type = SdpMessage.StringToType(type), Content = sdp };
+        }
+        public string type { get; set; }
+        public string sdp { get; set; }
+    }
+}
